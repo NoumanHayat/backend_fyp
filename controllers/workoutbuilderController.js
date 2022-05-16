779db057de47b8d1c51106d4b25bb099fcd02f0e
NoumanHayat <muhammadnoumanhayat@gmail.com>
2022-05-17 01:42:36 +0500

    before workoutbuilder

--- controllers/workoutbuilderController.js
+++ controllers/workoutbuilderController.js
@@ -1,13 +1,23 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const workoutbuilder = require("./../models/workoutBuilderModels");
 
 const jwt = require("jsonwebtoken");
-const User = require("../models/useModels");
 
 exports.addWorkout = catchAsync(async (req, res, next) => {
-  
+  var nowDate = new Date();
+  const newWorkoutbuilder = await workoutbuilder.create({
+    WorkoutType: {
+      UserId: req.user.id,
+      workoutName: req.body.workout,
+      targetMuscle: req.body.targetMuscle,
+      Date:nowDate,
+      intensity:req.body.intensity
+    },
+  });
+
   res.status(200).json({
     status: "success",
-    message: "Meal is added successfully!",
+    message: newWorkoutbuilder,
   });
 });
--- routes/workoutbuilderRoutes.js
+++ routes/workoutbuilderRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const multer = require("multer");
 const workoutbuilder = require("../controllers/workoutbuilderController");
-
-
-//  const authController = require('./../controllers/authController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 console.clear()
 // const upload = multer({ dest: "/" });
 router.route("/addWorkout")
-    .post(workoutbuilder.addWorkout)
+    .post(authController.protect,workoutbuilder.addWorkout)
 
 //172.17.240.1
 module.exports = router;
